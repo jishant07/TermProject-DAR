717d4d26a67369473bf81da9be6b179740115eee
jishant07 <jishanta@gmail.com>
2022-04-05 18:21:42 -0400

    Added Files

--- TermProject-DAR/server.R
+++ TermProject-DAR/server.R
@@ -3,55 +3,69 @@ library(corrplot)
 library(dplyr)
 library(DT)
 
+# Reading data in the beginning.
 studentPerformanceData <- as.data.frame(read.csv("./dataset/student-por.csv",sep = ";",stringsAsFactors = TRUE))
+# Reads in data description written in the form of an CSV
 datasetDescription <- read.csv("./dataset/dataset_description.csv")
 
+#Themeing
 primary <- "#75f4f4"
 secondary <- "#fffd82"
 tertiary <- "#e84855"
 
 
+#Server here does all the calculations when requested by the UI elements.
+
 server <- function(input, output, session) {
   
   # Student Performance Variables
   
-  output$studentAge_Range <- renderPrint({range(studentPerformanceData$age)})
+  #Factor Column Names
   column_names <- colnames(studentPerformanceData[sapply(studentPerformanceData,class)=="factor"])
   
+  # Renders the Data Table using DT library
   output$data_table <- DT::renderDataTable(
       studentPerformanceData,
       options = list(scrollX=TRUE)
     )
   
+  #Renders the summary of the data set.
   output$student_summary <- shiny::renderPrint({
     summary(studentPerformanceData)
   })
   
+  # Makes the Numeric Correlation Plot
   output$student_corrplot <- renderPlot({
     corrplot(cor(studentPerformanceData[sapply(studentPerformanceData, class) == "integer"]))
   })
   
+  #Generates a Dynamic UI selection
   output$x_axis_factor_columns <- renderUI({
     selectInput("x_axis", "X-Axis", column_names);
   })
   
+  #Generates a Dynamic UI selection
   output$y_axis_factor_columns <- renderUI({
     selectInput("y_axis", "Y-Axis", column_names);
   })
   
+  # Renders the dynamic distribution selection
   output$distribution_columns <- renderUI({
     selectInput("distribution_variable","Select the Variable You want to see the distribution of",column_names)
   })
   
+  # PLots the Distribution graph as requested
   output$distribution_plot <- renderPlot({
     column_selected <- input$distribution_variable
     ggplot(studentPerformanceData, aes_string(column_selected)) + geom_bar(fill=primary, color=tertiary) + 
       theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"))
   })
   
+  # Making the Input Variables available in UI
   output$x_axis_chisq <- renderText({input$x_axis})
   output$y_axis_chisq <- renderText({input$y_axis})
   
+  # Chi - Square Test Logic
   output$chi_sq_test_result <- renderPrint({
     x <- input$x_axis
     y <- input$y_axis
@@ -62,19 +76,20 @@ server <- function(input, output, session) {
       "Retaining Alternate Hypothesis"
     }
   })
-  
-  output$regression <- renderPlot({
-    ggplot(studentPerformanceData,aes_string(x=input$x_axis_regression, y=input$y_axis_regression)) + geom_point(color=tertiary) + 
-      geom_smooth(method = 'lm', color=primary) + 
-      theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"))
-  })
-  
   output$chi_sq_test <- renderPrint({
     x <- input$x_axis
     y <- input$y_axis
     chisq.test(as.matrix(studentPerformanceData[x]),as.matrix(studentPerformanceData[y]))
   })
   
+  #Dynamic Regression plot
+  output$regression <- renderPlot({
+    ggplot(studentPerformanceData,aes_string(x=input$x_axis_regression, y=input$y_axis_regression)) + geom_point(color=tertiary) + 
+      geom_smooth(method = 'lm', color=primary) + 
+      theme(axis.text=element_text(size=12),axis.title=element_text(size=14,face="bold"))
+  })
+  
+  # Renders the Table for data set description
   output$dataset_description <- renderTable({
     datasetDescription
   })
--- TermProject-DAR/ui.R
+++ TermProject-DAR/ui.R
@@ -1,22 +1,27 @@
 library(shiny)
 library(shinythemes)
 library(shinydashboard)
-library(knitr)
 
+
+#ui here acts as the view, it sets up the things as required on the page.
 ui <- dashboardPage(
   skin = "purple",
-  dashboardHeader(title="Data Analytics in R!"),
+  dashboardHeader(title="Data Analytics with R!"),
+  # Sets up the Dashboard's Sidebar
   dashboardSidebar(
     sidebarMenu(
       menuItem("Dataset Description",tabName = "dataset_description",icon = icon("dashboard")),
+      menuItem("Correlation Plot", tabName = "correlation", icon = icon("users", lib = "font-awesome")),
       menuItem("Factor Frequency Distributions",tabName = "factor_distributions",icon = icon("chart-bar",lib="font-awesome")),
       menuItem("Chi-Square test of Factors",tabName = "chi_sq",icon=icon("th")),
       menuItem("Regression", tabName = "regression", icon = icon("line-chart", lib="font-awesome")),
       menuItem("About",tabName = "about", icon = icon("child", lib="font-awesome"))
     )
   ),
+  # Sets up the Dashboard's body
   dashboardBody(
     tabItems(
+      # Data Set description Option's View
       tabItem(
         tabName = "dataset_description", 
         fluidPage(
@@ -51,6 +56,7 @@ ui <- dashboardPage(
           )
         )
       ),
+      # Factor Distributions Option's View
       tabItem(
         tabName = "factor_distributions",
         fluidPage(
@@ -70,6 +76,7 @@ ui <- dashboardPage(
           )
         )
       ),
+      # Chi - Square Tab option's View
       tabItem(
         tabName = "chi_sq",
         fluidPage(
@@ -79,10 +86,12 @@ ui <- dashboardPage(
           tags$hr(style="border-color: purple;"),
           fluidRow(
             column(
-              4, 
+              4,
+              # Renders the dynamic UI Elements of factor column names
               uiOutput("x_axis_factor_columns"),
               uiOutput("y_axis_factor_columns")
             ),
+            # Shows all the information about the test.
             column(
               8,
               fluidRow(
@@ -124,6 +133,7 @@ ui <- dashboardPage(
           )
         )
       ),
+      #Regression View
       tabItem(
         tabName="regression",
         fluidPage(
@@ -147,6 +157,23 @@ ui <- dashboardPage(
           )
         )
       ),
+      # Correlation Plot View
+      tabItem(
+        tabName = "correlation", 
+        fluidPage(
+          fluidRow(
+            tags$h2("NUMERIC CORRELATION PLOT")
+          ),
+          tags$hr(style="border-color: purple;"),
+          fluidRow(
+            column(
+              12,
+              plotOutput("student_corrplot")
+            )
+          )
+        )
+      ),
+      # About Tab Item View
       tabItem(
         tabName = "about",
         fluidPage(
@@ -155,6 +182,7 @@ ui <- dashboardPage(
           ),
           tags$hr(style="border-color: purple;"),
           fluidRow(
+            # Display's Image present in "WWW" folder
             column(
               4, 
               img(src = "me.jpeg")
